4ccadcbc56e685072c1e923bf14de4ed6e113587
Francois Best <github@francoisbest.com>
2023-09-18 20:50:40 +0200

    chore: Test different implementations

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,11 +1,11 @@
 import { NextResponse } from 'next/server'
 import fs from 'node:fs/promises'
-import { resolve } from '../../lib/paths'
+import { resolveViaProcessCwd } from '../../lib/paths'
 
 export const dynamic = 'force-dynamic'
 
 export async function GET() {
-  const filePath = resolve(import.meta.url, '../relative.txt')
+  const filePath = resolveViaProcessCwd(import.meta.url, '../relative.txt')
   console.log('[ROUTE] imr: ', import.meta.url)
   console.log('[ROUTE] filePath: ', filePath)
   const contents = await fs.readFile(filePath, 'utf-8')
--- src/lib/paths.ts
+++ src/lib/paths.ts
@@ -1,6 +1,20 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 
-export function resolve(importMetaUrl: string, ...paths: string[]) {
-  return fileURLToPath(new URL(path.join(...paths), importMetaUrl).toString());
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+export const nextJsRootDir = path.resolve(__dirname, '../../')
+
+export function resolveViaURL(importMetaUrl: string, ...paths: string[]) {
+  return fileURLToPath(new URL(path.join(...paths), importMetaUrl).toString())
+}
+
+export function resolveViaProcessCwd(
+  importMetaUrl: string,
+  ...paths: string[]
+) {
+  const dirname = path.dirname(fileURLToPath(importMetaUrl))
+  const absPath = path.resolve(dirname, ...paths)
+  // Required for ISR serverless functions to pick up the file path
+  // as a dependency to bundle.
+  return path.resolve(process.cwd(), absPath.replace(nextJsRootDir, '.'))
 }
