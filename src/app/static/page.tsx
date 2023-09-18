e92dde8ffce7ff913ef217f7c8f003a6136fc7d8
Francois Best <github@francoisbest.com>
2023-09-18 20:47:00 +0200

    chore: Enable ISR

--- src/app/static/page.tsx
+++ src/app/static/page.tsx
@@ -1,10 +1,12 @@
-import fs from "node:fs/promises";
-import { resolve } from "../../lib/paths";
+import fs from 'node:fs/promises'
+import { resolve } from '../../lib/paths'
+
+export const revalidate = 30
 
 export default async function StaticPage() {
-  const filePath = resolve(import.meta.url, "../relative.txt");
-  console.log("[STATIC] imr: ", import.meta.url);
-  console.log("[STATIC] filePath: ", filePath);
-  const contents = await fs.readFile(filePath, "utf-8");
-  return <p>{contents}</p>;
+  const filePath = resolve(import.meta.url, '../relative.txt')
+  console.log('[STATIC] imr: ', import.meta.url)
+  console.log('[STATIC] filePath: ', filePath)
+  const contents = await fs.readFile(filePath, 'utf-8')
+  return <p>{contents}</p>
 }
